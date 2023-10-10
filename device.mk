d20c1197eb2ca9553921c50840d9e4634b94104c
perilouspike <shibushaji199818@gmail.com>
2023-10-11 02:18:58 +0530

    first commit

---
+++ device.mk
@@ -0,0 +1,21 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := device/ulefone/Armor_10_5G
+
+# Dynamic Partition
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 29
+
+# API
+PRODUCT_SHIPPING_API_LEVEL := 29
+
+# Fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock
